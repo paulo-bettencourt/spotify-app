8383cc47997730e2ea41ff89585d82dd24332223
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-12-10 00:32:28 +0000

    api mergemap update

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { map, mergeMap, Observable } from 'rxjs';
 
@@ -7,22 +7,23 @@ import { map, mergeMap, Observable } from 'rxjs';
 })
 export class ApiService {
   private http = inject(HttpClient);
-  private CLIENT_ID = '50d42786e52940f78987dec2b26a8810'; // Replace with your Spotify API client ID
-  private CLIENT_SECRET = '475518206a27420da2676d6bf240779b'; // Replace with your Spotify API client secret
-  private bearerKey = 'NO_INITIAL_VALUE';
-  private apiUrl = 'https://open.spotify.com';
-  private headersTrackInfo = new HttpHeaders;
+  private CLIENT_ID = '50d42786e52940f78987dec2b26a8810';
+  private CLIENT_SECRET = '475518206a27420da2676d6bf240779b';
+  private LAST_FM_API_KEY = '20dd22a56594733b14fecd57de7a42ec';
+  private bearerKey!: string;
 
   getBearerKey() {
     const url = 'https://accounts.spotify.com/api/token';
-    const body = `grant_type=client_credentials&client_id=${encodeURIComponent(
-      this.CLIENT_ID
-    )}&client_secret=${encodeURIComponent(this.CLIENT_SECRET)}`;
+    const paramsString = new HttpParams()
+    .set('grant_type', 'client_credentials')
+    .set('client_id', this.CLIENT_ID)
+    .set('client_secret', this.CLIENT_SECRET)
+    .toString();
     const headers = new HttpHeaders({
       'Content-Type': 'application/x-www-form-urlencoded',
     });
 
-    return this.http.post(url, body, { headers })
+    return this.http.post(url, paramsString, { headers })
   }
 
   private getHeaders(): HttpHeaders {
@@ -37,11 +38,9 @@ export class ApiService {
     return this.getBearerKey().pipe(
       mergeMap((response: any) => {
         this.bearerKey = response.access_token;
-        this.headersTrackInfo = this.getHeaders();
-        return this.http.get(url, { headers: this.headersTrackInfo });
+        return this.http.get(url, { headers: this.getHeaders() });
       }),
       mergeMap((trackInfo: any) => {
-        // Use mergeMap to handle the observable returned by getArtistBiography
         return this.getArtistBiography(trackInfo.artists[0].name).pipe(
           map((artistBio: any) => {
             return {
@@ -54,10 +53,15 @@ export class ApiService {
     );
   }
 
-
   getArtistBiography(artist: string) {
-    const url = `https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artist}&api_key=20dd22a56594733b14fecd57de7a42ec&format=json`;
-    return this.http.get(url);
+    const url = 'https://ws.audioscrobbler.com/2.0/';
+    const params = new HttpParams()
+      .set('method', 'artist.getinfo')
+      .set('artist', artist)
+      .set('api_key', this.LAST_FM_API_KEY)
+      .set('format', 'json');
+
+    return this.http.get(url, { params });
   }
 }
 
