7cf392ce102c9c1a299984497e5dfd1643de7863
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-12-12 12:19:37 +0000

    fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,10 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { provideStore } from '@ngrx/store';
 
 import { routes } from './app.routes';
+import { tracksReducer } from './store/tracks/tracks.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +14,6 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(
       withFetch(),
     ),
-  //  provideStore({bioAndTrackstracks: tracksReducer}),
+  provideStore({bioAndTrackstracks: tracksReducer}),
   ]
 };
