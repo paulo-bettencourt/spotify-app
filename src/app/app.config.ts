3f564004b55e8b205ed363a5a1d7fb53d35d30e7
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-12-12 14:42:42 +0000

    commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,8 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { provideStore } from '@ngrx/store';
 
 import { routes } from './app.routes';
-import { tracksReducer } from './store/tracks/tracks.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,6 +12,5 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(
       withFetch(),
     ),
-    provideStore({biographyAndtracks: tracksReducer}),
   ]
 };
--- src/app/components/tracks/tracks.component.ts
+++ src/app/components/tracks/tracks.component.ts
@@ -7,7 +7,6 @@ import { Store } from '@ngrx/store';
 
 import { ApiService } from '../../services/api.service';
 import { BiographyAndTracks } from '../../shared/interfaces/tracks-biography.interface';
-import { BiographyAndTracksApiActions } from '../../store/tracks/tracks.actions';
 
 @Component({
   selector: 'spotify-tracks',
@@ -23,23 +22,14 @@ export class TracksComponent {
   store = inject(Store);
   searchValue!: string;
   tracksAndBiography = toSignal<BiographyAndTracks>(
-    this.store.select()
+    this.apiService.searchSpotify("Madonna"),
+    { injector: this.injector }
   );
 
   searchArtist(artist: string) {
-    /*     this.tracksAndBiography = toSignal<BiographyAndTracks>(
+       this.tracksAndBiography = toSignal<BiographyAndTracks>(
       this.apiService.searchSpotify(artist),
       { injector: this.injector }
-    ); */
-
-    this.apiService
-      .searchSpotify(artist)
-      .subscribe((biographyAndtracks) =>
-        this.store.dispatch(
-          BiographyAndTracksApiActions.retrievedBiographyAndTracks({
-            biographyAndtracks,
-          })
-        )
-      );
+    );
   }
 }
