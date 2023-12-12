066e9fc62439dfc726f04c597cd8d621174e8727
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-12-12 10:01:28 +0000

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { TracksComponent } from './components/tracks/tracks.component';
+
+export const routes: Routes = [{
+  path: '',
+  component: TracksComponent
+}];
