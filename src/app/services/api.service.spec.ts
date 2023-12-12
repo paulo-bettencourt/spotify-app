cdc03dfef48a386895e2c2f7dace6f5273dd3d2c
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-12-12 21:57:22 +0000

    jest setup

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { ApiService } from './api.service';
@@ -6,7 +7,9 @@ describe('ApiService', () => {
   let service: ApiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [HttpClient, HttpHandler]
+    });
     service = TestBed.inject(ApiService);
   });
 
