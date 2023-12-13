3bab4e25fc489d60497cc10253ef1c2c895bc573
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-12-13 18:11:19 +0000

    jest works fix

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -2,7 +2,9 @@ import 'jest';
 
 import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs';
 
+import { BiographyAndTracks } from '../shared/interfaces/tracks-biography.interface';
 import { ApiService } from './api.service';
 
 describe('ApiService', () => {
@@ -27,4 +29,35 @@ describe('ApiService', () => {
     expect(spyServiceGetHeaders).toHaveBeenCalled();
     expect(headersResult).toBeInstanceOf(HttpHeaders);;
   });
+
+  it('should get the artist\'s biography', () => {
+    const spyServiceGetHeaders = jest.spyOn(service, 'getArtistBiography');
+    const mockArtist = "Madonna";
+    const observableResult = service.getArtistBiography(mockArtist);
+    expect(spyServiceGetHeaders).toHaveBeenCalled();
+    expect(observableResult).toBeInstanceOf(Observable<BiographyAndTracks>);
+  });
+
 });
+
+/*
+
+  searchSpotify(artist: string): Observable<BiographyAndTracks> {
+    const url = environment.URL.SPOTIFY_SEARCH;
+    const params = new HttpParams().set('q', artist).set('type', 'track');
+
+    return this.getBearerKey().pipe(
+      mergeMap((response: Token) => {
+        this.bearerKey = response.access_token;
+        return this.http.get<Spotify>(url, { headers: this.getHeaders(), params });
+      }),
+      mergeMap((tracksFromRequest: Spotify) => {
+        return this.getArtistBiography(artist).pipe(
+          map((biographyFromRequest: LastFM) => {
+            return {
+              biography: biographyFromRequest,
+              tracks: tracksFromRequest
+            }
+          })
+        )
+*/
